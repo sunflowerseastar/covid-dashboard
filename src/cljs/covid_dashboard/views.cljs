e0dc3deed7162b382897b9c3829d9036688fb078
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-10 15:37:09 -0700

    add oz/vega map

--- src/cljs/covid_dashboard/views.cljs
+++ src/cljs/covid_dashboard/views.cljs
@@ -45,6 +45,40 @@
               :color {:field "col" :type "nominal"}}
    :mark "line"})
 
+(def map-test {:$schema "https://vega.github.io/schema/vega/v5.json"
+               :description "A choropleth map depicting U.S. unemployment rates by county in 2009."
+               :width 960
+               :height 500
+               :autosize "none"
+
+               :data [{:name "unemp"
+                       :url "https://covid-dashboard.sunflowerseastar.com/data/unemployment.tsv"
+                       :format { :type "tsv" :parse "auto"}}
+                      {:name "counties"
+                       :url "https://covid-dashboard.sunflowerseastar.com/data/us-10m.json"
+                       :format { :type "topojson" :feature "counties"}
+                       :transform [{:type "lookup" :from "unemp" :key "id" :fields ["id"] :values ["rate"] }
+                                   {:type "filter" :expr "datum.rate != null" }]}]
+
+               :projections [{:name "projection" :type "albersUsa"}]
+
+               :scales [{:name "color"
+                         :type "quantize"
+                         :domain [0 0.15]
+                         :range { :scheme "blues" :count 7}}]
+
+               :legends [{:fill "color"
+                          :orient "bottom-right"
+                          :title "Unemployment"
+                          :format "0.1%"}]
+
+               :marks [{:type "shape"
+                        :from { :data "counties"}
+                        :encode {:enter { :tooltip { :signal "format(datum.rate, '0.1%')" }}
+                                 :update {:fill { :scale "color" :field "rate"} }
+                                 :hover {:fill { :value "red"} }}
+                        :transform [{:type "geoshape" :projection "projection" }]}]})
+
 (defn home-panel []
   [re-com/h-box
    :class "middle"
@@ -54,6 +88,7 @@
                :child [:div
                        [home-title]
                        [oz.core/vega-lite line-plot]
+                       [oz.core/vega map-test]
                        [link-to-about-page]
                        [:div
                         [:h3 (str "screen-width: " @(re-frame/subscribe [::bp/screen-width]))]
