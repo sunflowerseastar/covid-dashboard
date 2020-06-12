fff438df158c9bdb4bf65b6d5613d6e1e8f33a97
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-12 02:26:28 -0700

    failed attempt to fix vegaTooltip notices in console
    TypeError: vegaTooltip.vegaLite is not a function
        at eval (core.cljs:32)
    02:26:14.074 core.cljs:20 TypeError: vegaTooltip.vega is not a function
        at eval (core.cljs:44)
    02:26:14.318 core.cljs:20 TypeError: vegaTooltip.vega is not a function
        at eval (core.cljs:44)

--- src/cljs/deps.cljs
+++ src/cljs/deps.cljs
@@ -1,9 +1,10 @@
-{:npm-deps     {"d3" "5.12.0"}
+{:npm-deps {"d3" "5.12.0"
+            "vega" "5.13.0"
+            "vega-embed" "6.9.0"
+            "vega-lite" "4.13.1"
+            "vega-tooltip" "0.23.0"}
  :npm-dev-deps {"karma" "4.4.1"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test" "0.1.0"
                 "karma-junit-reporter" "2.0.1"
-                "shadow-cljs" "2.9.3"
-                "vega" "5.13.0"
-                "vega-embed" "6.9.0"
-                "vega-lite" "4.13.1"}}
+                "shadow-cljs" "2.9.3"}}
