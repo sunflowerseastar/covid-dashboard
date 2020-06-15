ca18d3eb0aa7ab0a380ed3a79c7b1682feb97626
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-15 13:45:45 -0700

    remove unused d3 visualizations

--- src/cljs/covid_dashboard/views.cljs
+++ src/cljs/covid_dashboard/views.cljs
@@ -3,7 +3,7 @@
    [breaking-point.core :as bp]
    [covid-dashboard.subs :as subs]
    [covid-dashboard.vegas :refer [line-plot-vega-lite map-us-chloropleth-vega]]
-   [covid-dashboard.d3s :refer [line-chart-d3 bubble-map-population-d3 bubble-map-unemployment-us-d3 bubble-map-covid-us-d3]]
+   [covid-dashboard.d3s :refer [bubble-map-covid-us-d3]]
    [oz.core :as oz]
    [re-com.core :as re-com]
    [re-frame.core :as re-frame]
@@ -15,16 +15,16 @@
   [re-com/v-box
    :class "home-col-left"
    :children [[re-com/box :size "1" :child [bubble-map-covid-us-d3]]
-              [re-com/box :size "1" :child [line-chart-d3]]]])
+              [re-com/box :size "1" :child "x"]]])
 
 (defn home-col-center []
-  [:div.home-col-center [:div.d3-container [bubble-map-unemployment-us-d3]]])
+  [:div.home-col-center [:div.d3-container "x"]])
 
 (defn home-col-right []
   [re-com/v-box
    :class "home-col-right"
    :size "auto"
-   :children [[re-com/box :size "1" :child [bubble-map-population-d3]]
+   :children [[re-com/box :size "1" :child "x"]
               [re-com/box :size "1" :child [map-us-chloropleth-vega]]]])
 
 (defn home-panel []
