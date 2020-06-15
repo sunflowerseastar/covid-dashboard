a17749976ee6f4e83ae374198b7887deab5efd0d
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-15 00:05:08 -0700

    unemployment bubble map

--- src/cljs/covid_dashboard/d3s.cljs
+++ src/cljs/covid_dashboard/d3s.cljs
@@ -65,16 +65,16 @@
 
 (defn format [x] (.format js/d3 ",.0f" x))
 
-(def files ["https://covid-dashboard.sunflowerseastar.com/data/population.json" "https://covid-dashboard.sunflowerseastar.com/data/counties-albers-10m.json"])
+(def files-population ["https://covid-dashboard.sunflowerseastar.com/data/population.json" "https://covid-dashboard.sunflowerseastar.com/data/counties-albers-10m.json"])
 
-(defn bubble-map [starting-width]
-  (-> (.all js/Promise (map #(.json js/d3 %) files))
+(defn bubble-map-population [starting-width]
+  (-> (.all js/Promise (map #(.json js/d3 %) files-population))
       (.then
        (fn [[population-data counties-albers-10m-data]]
          (let [myGeoPath (geoPath)
                population-data-map (js/Map.)
                _ (dorun (map #(.set population-data-map (first %) (second %)) (js->clj population-data)))
-               svg (.. js/d3 (select "#bubble-map-d3-svg-root") (attr "width" starting-width) (attr "height" (* starting-width 0.6)))
+               svg (.. js/d3 (select "#bubble-map-population-d3-svg-root") (attr "width" starting-width) (attr "height" (* starting-width 0.6)))
 
                ;; TODO change hard-coded 1000000 to high-end of domain of populationDataMap values
                scale-radius #((.scaleSqrt js/d3 (clj->js [0 1000000]) (clj->js [0 10])) (or % 0))]
@@ -142,8 +142,93 @@
                ;; (.text (fn [d] (str (-> d .-properties .-name) " " (format (.-value d)))))
                ))))))
 
-(defn bubble-map-d3 []
+(defn bubble-map-population-d3 []
   (r/create-class
-   {:display-name "bubble-map-d3"
-    :reagent-render (fn [this] [:div#d3-bubble-map-container [:svg#bubble-map-d3-svg-root {:viewBox [0 0 975 610]} [:g.hello]]])
-    :component-did-mount #(bubble-map @(re-frame/subscribe [::bp/screen-width]))}))
+   {:display-name "bubble-map-population-d3"
+    :reagent-render (fn [this] [:div#d3-bubble-map-container [:svg#bubble-map-population-d3-svg-root {:viewBox [0 0 975 610]} [:g.hello]]])
+    :component-did-mount #(bubble-map-population (/ @(re-frame/subscribe [::bp/screen-width]) 3))}))
+
+(def files-covid ["https://covid-dashboard.sunflowerseastar.com/data/unemployment.tsv" "https://covid-dashboard.sunflowerseastar.com/data/counties-albers-10m.json"])
+
+(defn bubble-map-covid [starting-width]
+  (-> (.all js/Promise [(.tsv js/d3 (first files-covid) #(vector (.-id %) (.-rate %))) (.json js/d3 (second files-covid))])
+      (.then
+       (fn [[population-data counties-albers-10m-data]]
+         (let [myGeoPath (geoPath)
+               population-data-map (js/Map.)
+               _ (dorun (map #(.set population-data-map (first %) (second %)) (js->clj population-data)))
+               svg (.. js/d3 (select "#bubble-map-covid-us-d3-svg-root") (attr "width" starting-width) (attr "height" (* starting-width 0.6)))
+
+               ;; TODO change hard-coded 1000000 to high-end of domain of populationDataMap values
+               scale-radius #((.scaleSqrt js/d3 (clj->js [0 1]) (clj->js [0 20])) (or % 0))]
+
+           (.log js/console population-data population-data-map)
+
+           (-> svg
+               (.append "path")
+               (.datum (feature counties-albers-10m-data (-> counties-albers-10m-data .-objects .-nation)))
+               (.attr "fill", "#ccc")
+               (.attr "d" myGeoPath))
+
+           (-> svg
+               (.append "path")
+               (.datum (mesh counties-albers-10m-data (-> counties-albers-10m-data .-objects .-states) (fn [a b] (not= a b))))
+               (.attr "fill" "none")
+               (.attr "stroke" "white")
+               (.attr "stroke-linejoin" "round")
+               (.attr "d" myGeoPath))
+
+           ;; legend setup
+           (-> svg
+               (.append "g")
+               (.attr "fill" "#777")
+               (.attr "transform" (str "translate(50 100)"))
+               (.attr "text-anchor" "middle")
+               (.style "font" "10px sans-serif")
+               (.selectAll "g")
+               (.data (clj->js [1000000 5000000 10000000]))
+               (.join "g")
+               ;; (.call (fn [d] (do (.log js/console d))))
+               (.append "circle")
+               (.attr "fill" "none")
+               (.attr "stroke" "#f00")
+               (.attr "cy" #(* -1 (scale-radius %)))
+               (.attr "r" scale-radius))
+
+           ;; legend text
+           ;; (-> svg
+           ;;     (.append "text")
+           ;;     (.attr "y" (fn [d] (* -2 (scale-radius d))))
+           ;;     (.attr "dy" "1.3em")
+           ;;     (.text (format ".1s")))
+
+           ;; marks
+           (-> svg
+               (.append "g")
+
+               (.attr "fill" "purple")
+               (.attr "fill-opacity" 0.2)
+               (.attr "stroke" "#fff")
+               (.attr "stroke-width" 0.5)
+
+               (.selectAll "circle")
+               (.data (->> (feature counties-albers-10m-data (-> counties-albers-10m-data .-objects .-counties))
+                           (.-features)
+                           (js->clj)
+                           (map #(assoc % :value (.get population-data-map (get % "id"))))
+                           (sort-by :value)
+                           (clj->js)))
+               (.join "circle")
+               (.attr "transform" #(str "translate(" (.centroid myGeoPath %) ")"))
+               (.attr "r" #(scale-radius (.-value %)))
+
+               ;; TODO title
+               ;; (.append "title")
+               ;; (.text (fn [d] (str (-> d .-properties .-name) " " (format (.-value d)))))
+               ))))))
+
+(defn bubble-map-covid-us-d3 []
+  (r/create-class
+   {:display-name "bubble-map-covid-us-d3"
+    :reagent-render (fn [this] [:div#d3-bubble-map-container [:svg#bubble-map-covid-us-d3-svg-root {:viewBox [0 0 975 610]} [:g.hello]]])
+    :component-did-mount #(bubble-map-covid (/ @(re-frame/subscribe [::bp/screen-width]) 3))}))
--- src/cljs/covid_dashboard/views.cljs
+++ src/cljs/covid_dashboard/views.cljs
@@ -3,7 +3,7 @@
    [breaking-point.core :as bp]
    [covid-dashboard.subs :as subs]
    [covid-dashboard.vegas :refer [line-plot-vega-lite map-us-chloropleth-vega]]
-   [covid-dashboard.d3s :refer [line-chart-d3 bubble-map-d3]]
+   [covid-dashboard.d3s :refer [line-chart-d3 bubble-map-population-d3 bubble-map-covid-us-d3]]
    [oz.core :as oz]
    [re-com.core :as re-com]
    [re-frame.core :as re-frame]
@@ -18,13 +18,13 @@
               [re-com/box :size "1" :child [line-chart-d3]]]])
 
 (defn home-col-center []
-  [:div.home-col-center [:div.d3-container [bubble-map-d3]]])
+  [:div.home-col-center [:div.d3-container [bubble-map-covid-us-d3]]])
 
 (defn home-col-right []
   [re-com/v-box
    :class "home-col-right"
    :size "auto"
-   :children [[re-com/box :size "1" :child "right top"]
+   :children [[re-com/box :size "1" :child [bubble-map-population-d3]]
               [re-com/box :size "1" :child [map-us-chloropleth-vega]]]])
 
 (defn home-panel []
@@ -38,8 +38,8 @@
 
 (defn- panels [panel-name]
   (case panel-name
-    ;; :home-panel [home-panel]
-    :home-panel [:div.d3-container [bubble-map-d3]]
+    :home-panel [home-panel]
+    ;; :home-panel [:div.d3-container [bubble-map-population-d3]]
     [:div]))
 
 (defn show-panel [panel-name]
