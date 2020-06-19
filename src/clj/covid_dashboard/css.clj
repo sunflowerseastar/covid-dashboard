65af2c96bb7a64d5f95568461082ee2ada1fc241
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-19 13:50:11 -0700

    constrain panel dimensions

--- src/clj/covid_dashboard/css.clj
+++ src/clj/covid_dashboard/css.clj
@@ -3,4 +3,12 @@
 
 (defstyles screen
   [:.home-page {:height "100vh"}]
+
+  [:.display-flex {:width "100%"}]
+
+  [:.home-col-left {:background "yellow"}]
+
+  [:.panel-2 {:background "#eee"}]
+  [:.panel-2-scroll {:overflow-y "auto"}]
+
   [:.svg-container {:position "relative" :top "50%" :transform "translateY(-50%)" :width "100%"}])
--- src/cljs/covid_dashboard/views.cljs
+++ src/cljs/covid_dashboard/views.cljs
@@ -15,27 +15,35 @@
 (defn panel-2 []
   (let [confirmed-by-region (re-frame/subscribe [::subs/confirmed-by-region])]
     (when @confirmed-by-region
-      [:div.panel-2 [:p "Confirmed Cases by Country/Region/Sovereignty"]
-       [:table [:tbody (map (fn [[region value]]
-                              [:tr {:key (str region value)} [:td region] [:td value]])
-                            @confirmed-by-region)]]])))
+      [re-com/v-box :class "panel-2" :size "1"
+       :children
+       [[re-com/box :child [:p "Confirmed Cases by Country/Region/Sovereignty"]]
+        [re-com/box :size "1" :class "panel-2-scroll"
+         :child [:table [:tbody (map (fn [[region value]]
+                                       [:tr {:key (str region value)} [:td region] [:td value]])
+                                     @confirmed-by-region)]]]]])))
 
 (defn panel-4 []
   (let [global-deaths (re-frame/subscribe [::subs/global-deaths])]
     (when-let [{:keys [deaths-by-region total-deaths]} @global-deaths]
-      [:div.panel-4 [:p "Global Deaths"]
-       [:h3 total-deaths]
-       [:table [:tbody (map (fn [[region value]]
-                              [:tr {:key (str region value)} [:td region] [:td value]])
-                            deaths-by-region)]]])))
+      [re-com/v-box :class "panel-2" :size "1"
+       :children
+       [[re-com/box :child [:div [:p "Global Deaths"] [:h3 total-deaths]]]
+        [re-com/box :size "1" :class "panel-2-scroll"
+         :child [:table [:tbody (map (fn [[region value]]
+                                       [:tr {:key (str region value)} [:td region] [:td value]])
+                                     deaths-by-region)]]]]])))
 
 (defn panel-5 []
   (let [us-state-level-deaths-recovered (re-frame/subscribe [::subs/us-state-level-deaths-recovered])]
     (when @us-state-level-deaths-recovered
-      [:div.panel-2 [:p "US State Level"] [:h4 "Deaths, Recovered"]
-       [:table [:tbody (map (fn [[state deaths recovered]]
-                              [:tr {:key state} [:td state] [:td deaths] [:td recovered]])
-                            @us-state-level-deaths-recovered)]]])))
+      [re-com/v-box :class "panel-2" :size "1"
+       :children
+       [[re-com/box :child [:div [:p "US State Level"] [:h4 "Deaths, Recovered"]]]
+        [re-com/box :size "1" :class "panel-2-scroll"
+         :child [:table [:tbody (map (fn [[state deaths recovered]]
+                                       [:tr {:key state} [:td state] [:td deaths] [:td recovered]])
+                                     @us-state-level-deaths-recovered)]]]]])))
 
 (defn home-col-left []
   [re-com/v-box
@@ -62,6 +70,6 @@
        :height "100%"
        :children [[re-com/h-box
                    :class "home-page"
-                   :children [[re-com/box :size "1" :child [home-col-left]]
-                              [re-com/box :size "3" :class "home-col-center" :child [d3s/bubble-map-covid-us-d3]]
-                              [re-com/box :size "1" :child [home-col-right]]]]]])}))
+                   :children [[re-com/box :size "2" :child [home-col-left]]
+                              [re-com/box :size "5" :class "home-col-center" :child [d3s/bubble-map-covid-us-d3]]
+                              [re-com/box :size "3" :child [home-col-right]]]]]])}))
