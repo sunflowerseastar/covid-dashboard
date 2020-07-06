ff80b5ff5d14ac5f3a32667911ddd19d609b0c79
Grant Surlyn <grant@sunflowerseastar.com>
2020-07-06 11:40:04 -0700

    cleanup

--- src/cljs/covid_dashboard/components.cljs
+++ src/cljs/covid_dashboard/components.cljs
@@ -8,14 +8,17 @@
    [reagent.core :refer [atom with-let]]))
 
 (defn sub-panel-container-mobile
-  "Takes a vector of title-component pairs, returns a fading v-box 'switcher' panel"
+  "Takes a vector of title-component pairs, returns a v-box with a display on top and a switcher on bottom.
+  The display fades when switching."
   [sub-panels]
   (with-let [curr (atom 0) is-transitioning (atom false) sub-panel-count (count sub-panels)
              update-map #(do (reset! is-transitioning true)
                              (js/setTimeout (fn [] (swap! curr %)) duration-2)
                              (js/setTimeout (fn [] (reset! is-transitioning false)) (* duration-2 1.5)))]
     [v-box :size "1" :children
-     [[box :size "1" :class (str "justify-content-center fade-duration-2 " (if @is-transitioning "is-inactive" "is-active")) :child [(-> (get sub-panels @curr) second)]]
+     [;; display
+      [box :size "1" :class (str "justify-content-center fade-duration-2 " (if @is-transitioning "is-inactive" "is-active")) :child [(-> (get sub-panels @curr) second)]]
+      ;; switcher
       [box :size (if (= @(re-frame/subscribe [::bp/screen]) :mobile) control-bar-height control-bar-height-desktop) :child
        [h-box :size "1" :class "children-align-self-center z-index-1" :children
         [[box :child [:a.button {:on-click #(when (not @is-transitioning) (update-map dec))} "←"]]
@@ -24,18 +27,24 @@
          [box :child [:a.button {:on-click #(when (not @is-transitioning) (update-map inc))} "→"]]]]]]]))
 
 (defn sub-panel-container
-  "Takes a vector of title-component pairs, returns a v-box 'switcher' panel"
+  "Takes a vector of title-component pairs, returns a v-box with a display on top and a switcher on bottom.
+  Switching state is local."
   [sub-panels]
   (with-let [curr (atom 0) sub-panel-count (count sub-panels)]
     [v-box :size "1" :children
-     [[box :size "1" :class "justify-content-center" :child [(-> (get sub-panels @curr) second)]]
+     [;; display
+      [box :size "1" :class "justify-content-center" :child [(-> (get sub-panels @curr) second)]]
+      ;; switcher
       [box :size (if (= @(re-frame/subscribe [::bp/screen]) :mobile) control-bar-height control-bar-height-desktop) :child
        [h-box :size "1" :class "children-align-self-center z-index-1" :children
         [[box :child [:a.button {:on-click #(reset! curr (if (= (dec @curr) -1) (dec sub-panel-count) (dec @curr)))} "←"]]
          [box :size "1" :child [:p.margin-0-auto (-> (get sub-panels @curr) first) [:span.light (str " — " (inc @curr) "/" sub-panel-count)]]]
          [box :child [:a.button {:on-click #(reset! curr (if (= (inc @curr) sub-panel-count) 0 (inc @curr)))} "→"]]]]]]]))
 
-(defn map-switcher [sub-panels]
+(defn map-switcher
+  "Takes a vector of title-component vector pairs, returns a v-box with a spacer on top, an info panel
+  that shows & hides, and a switcher. Switching state is global."
+  [sub-panels]
   (with-let [sub-panel-count (count sub-panels)
              curr-map (re-frame/subscribe [::subs/curr-map])
              is-transitioning (re-frame/subscribe [::subs/is-transitioning])
@@ -43,7 +52,9 @@
                              (js/setTimeout (fn [] (re-frame/dispatch [:update-curr-map %])) duration-2)
                              (js/setTimeout (fn [] (re-frame/dispatch [:assoc-is-transitioning false])) (* duration-2 1.5)))]
     [v-box :size "1" :children
-     [[box :size "1" :child ""]
+     [;; spacer
+      [box :size "1" :child ""]
+      ;; info panel
       [box :child (let [county (re-frame/subscribe [:active-county])
                         country (re-frame/subscribe [:active-country])
                         value (re-frame/subscribe [:active-value])]
@@ -52,6 +63,7 @@
                        (when @county [:p [:span.bold "County: "] @county])
                        (when @country [:p [:span.bold "Country: "] @country])
                        [:p [:span.bold "Value: "] @value]] ""))]
+      ;; switcher
       [box :size control-bar-height-desktop :child
        [h-box :size "1" :class "children-align-self-center z-index-1 panel" :children
         [[box :child [:a.button {:on-click #(when (not @is-transitioning) (do (re-frame/dispatch [:clear-actives])
