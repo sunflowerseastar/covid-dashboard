94d2015c058106e3ccaacabe94e632a9b494a8e1
Grant Surlyn <grant@sunflowerseastar.com>
2021-01-23 15:09:19 -0500

    add spec

--- src/cljs/covid_dashboard/db.cljs
+++ src/cljs/covid_dashboard/db.cljs
@@ -1,4 +1,14 @@
-(ns covid-dashboard.db)
+(ns covid-dashboard.db
+  (:require [cljs.spec.alpha :as s]))
+
+(s/def ::detail-country (s/nilable string?))
+(s/def ::detail-county (s/nilable string?))
+(s/def ::detail-state (s/nilable string?))
+(s/def ::detail-value (s/nilable int?))
+(s/def ::confirmed-by-state (s/nilable (s/cat :confirmed-state-pairs (s/spec (s/cat :confirmed int? :state string?)))))
+(s/def ::total-confirmed (s/nilable int?))
+(s/def ::tick (s/nilable (s/and int? #(>= % 0))))
+(s/def ::db (s/keys :req-un [::total-confirmed ::tick]))
 
 (def default-db
   {:detail-country nil
